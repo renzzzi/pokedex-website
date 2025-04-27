42e35e2da903ab0614d374a29e7e259ff2a3ac7c
renzzzi <renzdavidespiritu2006@gmail.com>
2025-04-27 14:24:21 +0800

    Implement type filtering for pokemons

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ $(document).ready(function() {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
+
+    $(".type-tab").on("click", function() {
+        var type = $(this).data("type");
+        if (type === "all") {
+            $(".pokemon-item").show();
+        } else {
+            $(".pokemon-item").hide();
+            $(".pokemon-item[data-type='" + type + "']").show();
+        }
+    });
 });
